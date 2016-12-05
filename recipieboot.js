6a73c9880d7834ca0dd4964eccae136a4e303ad0
Christine-horrocks <splogy2001@hotmail.com>
2016-12-05 19:26:25 +0000

    Upload

---
+++ recipieboot.js
@@ -0,0 +1,56 @@
+/*alert("Kit is a fluffy bum and big brain");*/
+
+
+// alert('we did that thing');
+$( document ).ready(function() {
+
+
+
+
+	$(".tick-btn-1").click(function() {
+		$(".tick-btn-1").addClass("btn-success");
+	});
+
+	$(".tick-btn-2").click(function() {
+		$(".tick-btn-2").addClass("btn-success");
+	});
+
+
+	$(".tick-btn-3").click(function() {
+		$(".tick-btn-3").addClass("btn-success");
+	});
+
+
+	$(".tick-btn-4").click(function() {
+		$(".tick-btn-4").addClass("btn-success");
+	});
+
+	$(".tick-btn-5").click(function() {
+		$(".tick-btn-5").addClass("btn-success");
+	});
+
+	$(".tick-btn-6").click(function() {
+		$(".tick-btn-6").addClass("btn-success");
+	});
+
+	$(".tick-btn-7").click(function() {
+		$(".tick-btn-7").addClass("btn-success");
+	});
+
+	$(".tick-btn-8").click(function() {
+		$(".tick-btn-8").addClass("btn-success");
+	});
+
+	$(".tick-btn-9").click(function() {
+		$(".tick-btn-9").addClass("btn-success");
+	});
+
+	$(".tick-btn-10").click(function() {
+		$(".tick-btn-10").addClass("btn-success");
+	});
+
+	$(".tick-btn-11").click(function() {
+		$(".tick-btn-11").addClass("btn-success");
+	});
+
+});
